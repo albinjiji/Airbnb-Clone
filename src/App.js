8b98fa0720e6e76f19ef772f862743e5c3048eec
Albin <albinjiji4@gmail.com>
2022-03-30 12:52:54 +0530

    update App component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
-import Search from "./Components/Search/Search";
 import SearchPage from "./Components/SearchPage/SearchPage";
 import Footer from "./Components/Footer/Footer";
 
